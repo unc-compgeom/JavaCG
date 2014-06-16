fe0de66ef35bb047dbbf79ef4f2b56f93735f80a
Turner <turnerr91@gmail.com>
2014-06-16 14:03:43 -0400

    Updated Calipers. Still working on width

--- algorithms/Calipers.java
+++ algorithms/Calipers.java
@@ -9,26 +9,33 @@ import cg.Polygon;
 
 public class Calipers {
 	
+	/*
+	 * 
+	 * Rotating caliper algorithm input point set in 2D.  Output is maximum diameter of the convex hull
+	 * and the minimum width of the convex hull.
+	 * 
+	 */
+	
 	public static void doCalipers(PointSet points, Polygon hull, Polygon diamline, Polygon support1, Polygon support2) {
 		hull = getConvexHull(points, hull);
 		int i=0;
 		int j=1;
-		double diam = Double.POSITIVE_INFINITY;
+		double diam = -1;
+//		double width = Double.POSITIVE_INFINITY;
 		double tempdiam;
-		while(cwIntersection(0,j,hull,true)){
+		while(cwIntersection(0,j,hull)){
 			j++;
 		}
 		while(j!=0){
 			Point pi = hull.getPoint(i);
 			Point pj = hull.getPoint(j);
-			tempdiam = Math.sqrt(Math.hypot(((double)pi.getX()-(double)pj.getX()),((double)pi.getY()-(double)pj.getY())));
-			if(tempdiam<diam){
+			tempdiam = Math.pow((pi.getX()-pj.getX()), 2) + Math.pow((pi.getY()-pj.getY()), 2);
+			if(tempdiam>diam){
 				diam=tempdiam;
 				if(!diamline.isEmpty()){
 					diamline.remove();
 					diamline.remove();
 				}
-			/*
 				if(!support1.isEmpty()){
 					support1.remove();
 					support1.remove();
@@ -37,23 +44,17 @@ public class Calipers {
 					support2.remove();
 					support2.remove();
 				}
-			*/
 				diamline.addPoint(pi);
 				diamline.addPoint(pj);
 				CG.sleep();
-			/*	The support lines intersect the convex hull at this point
-			 * since they are simply perpendicular to the diameter
-				double slope = (((double)pi.getY()-(double)pj.getY())/((double)pi.getX()-(double)pj.getX()));
-				slope = -1/slope;
-				double bi = pi.getY()-(pi.getX()*slope);
-				double bj = pj.getY()-(pj.getX()*slope);
-				support1.addPoint(new PointComponent((int)pi.getX()+60,(int)((slope*(pi.getX()+60))+bi)));
-				support1.addPoint(new PointComponent((int)pi.getX()-60,(int)((slope*(pi.getX()-60))+bi)));
-				support2.addPoint(new PointComponent((int)pj.getX()+60,(int)((slope*(pj.getX()+60))+bj)));
-				support2.addPoint(new PointComponent((int)pj.getX()-60,(int)((slope*(pj.getX()-60))+bj)));
-			*/
+				Point vector = perpendicular(i,j,hull);
+				support1.add(new PointComponent(pi.getX()+vector.getX(),pi.getY()+vector.getY()));
+				support1.add(new PointComponent(pi.getX()-vector.getX(),pi.getY()-vector.getY()));
+				support2.add(new PointComponent(pj.getX()+vector.getX(),pj.getY()+vector.getY()));
+				support2.add(new PointComponent(pj.getX()-vector.getX(),pj.getY()-vector.getY()));
+				CG.sleep();
 			}
-			if(cwIntersection(i+1,j,hull,false) || i+1==j){
+			if(cwIntersection(i+1,j,hull)){
 				j=(j+1)%hull.numPoints();
 			}
 			else{
@@ -88,28 +89,49 @@ public class Calipers {
 		return leftmost;
 	}
 
-	private static boolean cwIntersection(int i, int j, Polygon hull, boolean first){
-		Point pi = hull.getPoint(i);
-		//Point pi1 = first ? hull.getLast() : hull.getPoint((i+1) % hull.numPoints());
-		int temp = (i-1)%hull.numPoints();
-		if (temp<0){temp+=hull.numPoints();}
-		Point pi1 = hull.getPoint(temp);
-		Point pj = hull.getPoint(j % hull.numPoints());
-		Point pj1 = hull.getPoint((j+1) % hull.numPoints());
-		double slopei = (((double)pi.getY()-(double)pi1.getY())/((double)pi.getX()-(double)pi1.getX()));
-		double slopej = (((double)pj.getY()-(double)pj1.getY())/((double)pj.getX()-(double)pj1.getX()));
-		double bi = (double)pi.getY()-((double)pi.getX()*slopei);
-		double bj = (double)pj.getY()-((double)pj.getX()*slopej);
-		if((slopej-slopei)==0){
-			return false;
-		}
-		double xint = (bi-bj)/(slopej-slopei);
-		double yint = bi + (slopei*xint);
-		double det = (((double)pj.getX() - (double)pi.getX()) * (yint - (double)pi.getY())) - (((double)pj.getY() - (double)pi.getY()) * (xint - (double)pi.getX()));
-		if(det<0){
-			return false;
-		}
-		return true;
+	private static boolean cwIntersection(int i, int j, Polygon hull){
+		int tempi = i==0 ? hull.numPoints()-1 : i-1;
+		Point Pa = new PointComponent(hull.getPoint(tempi));
+		Point Pb = new PointComponent(hull.getPoint(i));
+		Point Pd = new PointComponent(hull.getPoint((j+1)%hull.numPoints()));
+		Pd.setPoint(Pd.getX()-hull.getPoint(j).getX()+Pb.getX(), Pd.getY()-hull.getPoint(j).getY()+Pb.getY());
+		return Predicate.findOrientation(Pa, Pb, Pd)==Predicate.Orientation.CLOCKWISE;
 	}
+	
+	private static Point perpendicular(int i, int j, Polygon hull){
+		double [][]  perp1 = {{0.0,-1.0},{1.0,0.0}};
+		double [][]  vector1 = {{0},{0}};
+		double [][]  vector2 = {{0},{0}};
+		double [][]  vector3 = {{0},{0}};
+		double [][]  result1 = {{0},{0}};
+		if(cwIntersection(i+1,j,hull)){
+			i = i==0 ? hull.numPoints()-1 : i-1;
+		}
+		else{
+			j = j==0 ? hull.numPoints()-1 : j-1;
+		}
+		vector1[0][0] = hull.getPoint((i+1)%hull.numPoints()).getX()-hull.getPoint(i).getX();
+		vector1[1][0] = hull.getPoint((i+1)%hull.numPoints()).getY()-hull.getPoint(i).getY();
+		double dist = Math.sqrt(Math.pow((hull.getPoint((i+1)%hull.numPoints()).getX()-hull.getPoint(i).getX()), 2) + Math.pow((hull.getPoint((i+1)%hull.numPoints()).getY()-hull.getPoint(i).getY()), 2));
+		vector1[0][0] = vector1[0][0]/dist;
+		vector1[1][0] = vector1[1][0]/dist;
+		vector2[0][0] = hull.getPoint((j+1)%hull.numPoints()).getX()-hull.getPoint(j).getX();
+		vector2[1][0] = hull.getPoint((j+1)%hull.numPoints()).getY()-hull.getPoint(j).getY();
+		dist = Math.sqrt(Math.pow((hull.getPoint((j+1)%hull.numPoints()).getX()-hull.getPoint(j).getX()), 2) + Math.pow((hull.getPoint((j+1)%hull.numPoints()).getY()-hull.getPoint(j).getY()), 2));
+		vector2[0][0] = vector2[0][0]/dist;
+		vector2[1][0] = vector2[1][0]/dist;
+		vector3[0][0]=(vector1[0][0]+vector2[0][0])/2;
+		vector3[1][0]=(vector1[1][0]+vector2[1][0])/2;
+		for (int a = 0; a < 2; a++) {
+            for (int b = 0; b < 1; b++) {
+                for (int c = 0; c < 2; c++) {
+                    result1[a][b] += perp1[a][c] * vector3[c][b];
+                }
+            }
+        }
+		return new PointComponent((int)(result1[0][0]*90),(int)(result1[1][0]*90));
+	}
+	
+	
 	
 }
