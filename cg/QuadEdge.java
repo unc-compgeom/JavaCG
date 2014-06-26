901423121f269aa0c3967a9b052b66f6cf1d76fa
Vance Miller <vbmiller@cs.unc.edu>
2014-06-26 12:07:52 -0400

    more javadoc

--- cg/QuadEdge.java
+++ cg/QuadEdge.java
@@ -2,6 +2,11 @@ package cg;
 
 class QuadEdge {
 
+	/**
+	 * Creates an empty edge quartet.
+	 * 
+	 * @return the edge
+	 */
 	public static Edge makeEdge() {
 		Edge[] edges = new Edge[4];
 		edges[0] = new EdgeComponent();
@@ -23,6 +28,20 @@ class QuadEdge {
 		return edges[0];
 	}
 
+	/**
+	 * Splices two edges together or apart. Splice affects the two edge rings
+	 * around the origins of a and b, and, independently, the two edge rings
+	 * around the left faces of a and b. In each case, (i) if the two rings are
+	 * distinct, Splice will combine them into one, or (ii) if the two are the
+	 * same ring, Splice will break it into two separate pieces. Thus, Splice
+	 * can be used both to attach the two edges together, and to break them
+	 * apart.
+	 * 
+	 * @param a
+	 *            an edge
+	 * @param b
+	 *            an edge
+	 */
 	public static void splice(Edge a, Edge b) {
 		Edge alpha = a.oNext().rot();
 		Edge beta = b.oNext().rot();
@@ -39,6 +58,18 @@ class QuadEdge {
 		System.out.println("spliced " + a + " " + b);
 	}
 
+	/**
+	 * Creates a new QuadEdge collection that connects the destination of
+	 * <tt>a</tt> to the origin of <tt>b</tt>, so that all three have the same
+	 * left face after the connection is complete and the data pointers of the
+	 * new edge are set.
+	 * 
+	 * @param a
+	 *            an edge
+	 * @param b
+	 *            an edge
+	 * @return the new edge connecting <tt>a</tt> and <tt>b</tt>
+	 */
 	public static Edge connect(Edge a, Edge b) {
 		Edge e = makeEdge();
 		splice(e, a.lnext());
@@ -48,6 +79,12 @@ class QuadEdge {
 		return e;
 	}
 
+	/**
+	 * Turns an edge counterclockwise inside its enclosing quadrilateral.
+	 * 
+	 * @param e
+	 *            the edge to swap
+	 */
 	public static void swap(Edge e) {
 		Edge a = e.oPrev();
 		Edge b = e.sym().oPrev();
@@ -61,6 +98,12 @@ class QuadEdge {
 		System.out.println("swapped " + e);
 	}
 
+	/**
+	 * Disconnects edge e from the collection of edges.
+	 * 
+	 * @param e
+	 *            the edge to delete
+	 */
 	public static void deleteEdge(Edge e) {
 		splice(e, e.oPrev());
 		splice(e.sym(), e.sym().oPrev());
